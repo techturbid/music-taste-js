1c8804d7da65bb9f8fc3687af2684e97c7b396e4
pedroshaina <pedroshaina_@hotmail.com>
2020-04-08 00:00:08 -0300

    adding spotify api call

---
+++ src/config/index.js
@@ -0,0 +1,6 @@
+const _ = require("lodash");
+
+const defaults = require("./default.js");
+const config = require("./" + (process.env.NODE_ENV || "dev") + ".js");
+
+module.exports = _.merge({}, defaults, config);
